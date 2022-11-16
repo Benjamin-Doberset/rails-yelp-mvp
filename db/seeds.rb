107c078ed35aadf2b92ae067c9aec5e7c20c61b4
Benjamin Doberset <benjamin.doberset.pro@gmail.com>
2022-11-16 17:12:31 +0100

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+  )
+  restaurant.save!
+end
+puts 'Finished!'
+
+puts 'Creating 100 fake reviews...'
+100.times do
+
+  review = Review.new(
+    rating: (0..5).to_a.sample,
+    content: Faker::Quote.famous_last_words,
+    restaurant: Restaurant.all.sample
+  )
+  review.save!
+end
+puts 'Finished!'
